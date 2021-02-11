923d1cdd6d575e8fd3b31f856233524166aa7a2a
dani <dani.ramdhaani@gmail.com>
2021-02-11 08:31:30 +0700

    penambahan folder menu

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -13,7 +13,6 @@ class Auth extends CI_Controller
 
         $this->form_validation->set_rules('username', 'Username', 'trim|required');
         $this->form_validation->set_rules('password', 'Password', 'trim|required');
-        $this->form_validation->set_rules('ruangan', 'Ruangan', 'trim|required');
         if ($this->form_validation->run() == false) {
             $data['title']  = 'Login';
             $this->load->view('templates/auth_header', $data);
@@ -25,7 +24,7 @@ class Auth extends CI_Controller
         }
     }
 
-    private function _ecript($strData)
+    private function _decript($strData)
     {
         $strData = '-' . $strData;
         $Code = "1234567890";
@@ -41,35 +40,80 @@ class Auth extends CI_Controller
     {
         $username       =   $this->input->post('username');
         $password       =   $this->input->post('password');
+        $ruangan        =   $this->input->post('ruangan');
 
-        $login          = $this->db->get('Login')->result();
         $user           = $this->db->get_where('Login', ['cast(Username as varchar)=' => $username])->row_array();
-        $pwd_ecript     = $this->_ecript($user['Password']);
-        $password       = $this->db->get_where('Login', ['cast(Password as varchar)=' => $pwd_ecript])->row_array();
+        $pwd_decript     = $this->_decript($user['Password']);
 
+        if ($ruangan    == '0') {
+            $this->session->set_flashdata(
+                'message',
+                '<div class="alert alert-danger" role="alert">Ruangan belum dipilih</div>'
+            );
+            redirect('auth');
+        } else {
+            if ($user) {
+                if ($password == $pwd_decript) {
+                    $data = [
+                        'idpegawai' => $user['IdPegawai'],
+                        'ruangan'   => $ruangan
+                    ];
 
+                    $loginaplikasi           = $this->db->get_where('LoginAplikasi', ['IdPegawai' => $user['IdPegawai']])->row_array();
+                    if ($loginaplikasi) {
+                        $loginruangan = $this->db->query("SELECT * FROM LoginRuangan WHERE IdPegawai = '" . $user['IdPegawai'] . "' AND KdRuangan ='" . $ruangan . "'")->result();
 
-        if ($user) {
-            if ($password = $user['Password']) {
-                $data = [
-                    'username' => $user['Username']
-                ];
-                $this->session->set_userdata($data);
-                //$this->session->set_userdata('coba');
-                redirect('admin');
+                        if ($loginruangan) {
+                            if ($user['IdPegawai'] == "8888888888") {
+                                $this->session->set_userdata($data);
+                                //$this->session->set_userdata('coba');
+                                redirect('user');
+                            } else {
+                                $settingglobal = $this->db->query("SELECT * FROM SettingGlobal where prefix = 'BerlakuExpirasiPasswordLogin' AND Value = '1'")->result();
+                                if ($settingglobal) {
+                                    $loginexpired = $this->db->query("SELECT * FROM LoginExpired where IdPegawai = '" . $user['IdPegawai'] . "' AND datediff( day, GETDATE(), TglExpired ) >= 1000")->result();
+
+                                    if ($loginexpired) {
+                                        $this->session->set_flashdata(
+                                            'message',
+                                            '<div class="alert alert-danger" role="alert">Password Anda telah Kadaluarsa, Silahkan ganti Password Anda..!!</div>'
+                                        );
+                                        redirect('auth');
+                                    } else {
+                                        $this->session->set_userdata($data);
+                                        //$this->session->set_userdata('coba');
+                                        redirect('user');
+                                    }
+                                }
+                            }
+                        } else {
+                            $this->session->set_flashdata(
+                                'message',
+                                '<div class="alert alert-danger" role="alert">User tidak berhak mengakses ruangan ini</div>'
+                            );
+                            redirect('auth');
+                        }
+                    } else {
+                        $this->session->set_flashdata(
+                            'message',
+                            '<div class="alert alert-danger" role="alert">User tidak berhak mengakses ruangan ini</div>'
+                        );
+                        redirect('auth');
+                    }
+                } else {
+                    $this->session->set_flashdata(
+                        'message',
+                        '<div class="alert alert-danger" role="alert">Password Salah</div>'
+                    );
+                    redirect('auth');
+                }
             } else {
                 $this->session->set_flashdata(
                     'message',
-                    '<div class="alert alert-danger" role="alert">Password Salah</div>'
+                    '<div class="alert alert-danger" role="alert">Username Tidak Ada.</div>'
                 );
                 redirect('auth');
             }
-        } else {
-            $this->session->set_flashdata(
-                'message',
-                '<div class="alert alert-danger" role="alert">Username Tidak Ada.</div>'
-            );
-            redirect('auth');
         }
     }
 
--- application/controllers/User.php
+++ application/controllers/User.php
@@ -5,15 +5,11 @@ class User extends CI_Controller
 {
     public function index()
     {
-        $data['title'] = 'Profile';
-        //var_dump($this->session->userdata('username'));
-        $data['user'] = $this->db->get_where('users', ['username' =>
-        $this->session->userdata('username')])->row_array();
+        $data['datapegawai']    = $this->db->get_where('dataPegawai', ['IdPegawai' => $this->session->userdata('idpegawai')])->row_array();
+        $data['title']          = 'Profile';
+        $data['ruangan']        = $this->db->get_where('ruangan', ['KdRuangan' => $this->session->userdata('ruangan')])->row_array();
+        $data['menu']           = $this->db->get('ListMenuWeb')->result();
 
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/sidebar', $data);
-        $this->load->view('templates/topbar', $data);
-        $this->load->view('user/index', $data);
-        $this->load->view('templates/footer');
+        $this->load->view('menu/index', $data);
     }
 }
